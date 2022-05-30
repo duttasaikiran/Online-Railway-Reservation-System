0f2e73531710c674fa6a3f15452ff7368f9e26f1
duttasaikiran <duttasaikiran@gmail.com>
2022-05-30 16:12:31 +0530

    front end

---
+++ src/components/Error.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <h1>Thank you for choosing Pakistan Railway.</h1>
+    </div>
+  )
+}
+
+export default Error
---
+++ src/components/Showcase.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Showcase() {
+  return (
+    <div className='hero-container'>
+       <img src='https://www.desktopbackground.org/download/o/2010/05/28/24248_amtrak-hd-wallpaper-amtrak-images-train-new-wallpapers_1920x1200_h.jpg' alt='Train image'></img>
+      {/* <video src='/videos/Railway1.mp4' autoPlay loop muted /> */}
+      {/* <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+      </div> */}
+  
+    </div>
+ 
+  );
+}
+
+export default Showcase;
+
